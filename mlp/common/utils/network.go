2d160dde524bd9554a8b0fd3461f4b6dcd4bbd98
Salva Corts <salvacorts97@gmail.com>
2019-07-09 11:50:47 +0200

    Function to print a MLP

--- mlp/common/utils/network.go
+++ mlp/common/utils/network.go
@@ -1,11 +1,37 @@
 package utils
 
 import (
+	"fmt"
 	"math/rand"
+	"reflect"
+	"runtime"
 
 	mn "github.com/made2591/go-perceptron-go/model/neural"
 	mu "github.com/made2591/go-perceptron-go/util"
 )
+
+func getFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+// MLPtoString retruns a string that represent a mlp
+func MLPtoString(mlp *mn.MultiLayerNetwork) string {
+	out := ""
+
+	out += fmt.Sprintf("Lrate: %f\nT_func: %s\nT_func_d: %s\n",
+		mlp.L_rate, getFunctionName(mlp.T_func), getFunctionName(mlp.T_func_d))
+
+	for i, layer := range mlp.NeuralLayers {
+		out += fmt.Sprintf("Layer: %d - Length: %d\n", i, layer.Length)
+
+		for j, neuron := range layer.NeuronUnits {
+			out += fmt.Sprintf("\tNeuron: %d - Bias: %f - Delta: %f - Lrate: %f - Value: %f\n\t\tWeights (#%d): %v\n",
+				j, neuron.Bias, neuron.Delta, neuron.Lrate, neuron.Value, len(neuron.Weights), neuron.Weights)
+		}
+	}
+
+	return out
+}
 
 // PredictN the output for a set of patterns
 func PredictN(mlp *mn.MultiLayerNetwork, input []mn.Pattern) (out [][]float64) {
@@ -48,8 +74,13 @@ func AccuracyN(roundedPredictions []float64, actual []mn.Pattern) (int, float64)
 }
 
 // RandIntInRange returns a random integer in range [min, max)
-func RandIntInRange(min, max int, rgn *rand.Rand) int {
-	return rgn.Intn(max-min) + min
+func RandIntInRange(min, max int, rng *rand.Rand) int {
+	return rng.Intn(max-min) + min
+}
+
+// RandFloatInRange returns a float integer in range [min, max]
+func RandFloatInRange(min, max float64, rng *rand.Rand) float64 {
+	return (max-min)*rng.Float64() + min
 }
 
 // RemoveNeuron the given index i from the slice of neurons
